e0ef9291b170764355e467325f46f7f88ad8115a
Alexander-Golubski <algolubski@gmail.com>
2018-12-17 17:35:55 -0600

    initial commit

---
+++ src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
@@ -0,0 +1,44 @@
+package org.launchcode.cheesemvc.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+
+@Controller
+@RequestMapping("cheese")
+public class CheeseController {
+
+    static ArrayList<String> cheeses = new ArrayList<>();
+
+    // Request path: /cheese
+    @RequestMapping(value = "")
+    public String index(Model model) {
+
+        model.addAttribute("cheeses", cheeses);
+        model.addAttribute("title", "My Cheeses");
+        return "cheese/index";
+    }
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String displayAddCheeseForm(Model model) {
+        model.addAttribute("title", "Add Cheese");
+        return "cheese/add";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String processAddCheeseForm(@RequestParam String cheeseName) {
+        //Spring will look for a request parameter based on name of
+        //x in @RequestParam String x, and x must match y in
+        //<input type="text" name=y /> in add.html
+
+        cheeses.add(cheeseName);
+
+        //Redirect to /cheese because of @RequestMapping("cheese) above
+        //public class CheeseController
+        return "redirect:";
+    }
+}
